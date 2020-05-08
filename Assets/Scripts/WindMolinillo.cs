2a437c8899cce8ac97f0735d25fb2d772f24a2a4
Jota García García <jotagamedev@gmail.com>
2020-05-08 16:58:10 +0200

    Molinillo done. Most UI sprites added

---
+++ Assets/Scripts/WindMolinillo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindMolinillo : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, -1), WindManager.Instance.airFactor * 180 * Time.deltaTime);
+    }
+}
