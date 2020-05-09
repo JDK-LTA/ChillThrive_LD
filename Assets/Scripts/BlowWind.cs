2275105f37b12507481f53c564bec62edf282225
Fen <nicolasviolantfen@gmail.com>
2020-05-09 19:22:29 +0300

    wind control polished & interface wind

--- Assets/Scripts/BlowWind.cs
+++ Assets/Scripts/BlowWind.cs
@@ -5,33 +5,60 @@ using UnityEngine;
 
 public class BlowWind : MonoBehaviour
 {
-    bool isDragging = false;
-    bool changedDir = false;
+    bool alreadyCalculated = false;
+    bool mouseOver = false;
+    bool toRight = false;
 
     Vector2 lastMousePos;
     float diffTime = 0;
     public float maxPower;
 
-    private void OnMouseDown()
+    SpriteRenderer buttonBG;
+
+    private void Start()
     {
-        isDragging = true;
-        lastMousePos = Input.mousePosition; //todo que solo cuente el drag si ha salido del button
+        buttonBG = GetComponent<SpriteRenderer>();
     }
+
     private void OnMouseUp()
     {
-        if (!changedDir)
+        if (!alreadyCalculated && !mouseOver)
         {
             CalculateAndSend();
+            alreadyCalculated = true;            
         }
-        if (Input.GetMouseButton(0))
+
+        if (!Input.GetMouseButton(0))
         {
-            changedDir = true;
+            alreadyCalculated = false;
+            toRight = false;
+            diffTime = 0;
+            buttonBG.color = new Color(1, 1, 1, 0);
+        }
+    }
+
+    private void OnMouseDrag()
+    {
+        if (alreadyCalculated) return;
+
+        diffTime += Time.deltaTime;
+
+        if (Input.GetAxis("Mouse X") > 0f)
+        {
+            toRight = true;
         }
         else
         {
-            changedDir = false;
+            if (toRight) OnMouseUp();
+            toRight = false;
+            lastMousePos = Input.mousePosition;
+            diffTime = 0;
         }
+    }
 
+    private void OnMouseDown()
+    {
+        buttonBG.color = new Color(1, 1, 1, 0.5f);
     }
 
     private void CalculateAndSend()
@@ -39,21 +66,10 @@ public class BlowWind : MonoBehaviour
         Vector2 mousePos = Input.mousePosition;
 
         float xDelta = Mathf.Abs(lastMousePos.x - mousePos.x);
-        float mouseSpeed = xDelta / diffTime/100;
+        float mouseSpeed = xDelta / diffTime / 100;
         SendWind(mouseSpeed);
 
         diffTime = 0;
-        isDragging = false;
-    }
-
-    private void OnMouseDrag()
-    {
-        diffTime += Time.deltaTime;
-
-        if (Input.GetAxis("Mouse X") < 0f)
-        {
-            OnMouseUp();
-        }
     }
 
     void SendWind(float power)
@@ -62,10 +78,19 @@ public class BlowWind : MonoBehaviour
         {
             power = maxPower;
         }
-        WindManager.Instance.airFactor += (power*0.7f) / maxPower;
+        WindManager.Instance.airFactor += (power * 0.7f) / maxPower;
         if (WindManager.Instance.airFactor > 1)
         {
             WindManager.Instance.airFactor = 1;
         }
+    }
+    private void OnMouseOver()
+    {
+        mouseOver = true;
+    }
+
+    private void OnMouseExit()
+    {
+        mouseOver = false;
     }
 }
