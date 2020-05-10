c2b5d457732eb3ffddad0fed052c3a018b4adfe3
Jota García García <jotagamedev@gmail.com>
2020-05-10 18:42:02 +0200

    Tutorial

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    private void Start()
+    {
+        SetThisActive();
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            gameObject.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void SetThisActive()
+    {
+        gameObject.SetActive(true);
+        Time.timeScale = 0.25f;
+    }
+}
